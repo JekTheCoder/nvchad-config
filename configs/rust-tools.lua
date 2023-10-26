e2d15aa77ec19b754e8e6f60e44e1895600cdb98
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2023-10-25 23:18:10 -0500

    draft | rust special keybinding

--- configs/rust-tools.lua
+++ configs/rust-tools.lua
@@ -1,11 +1,16 @@
 local rust_tools = require "rust-tools"
 local plugings_lsp = require "plugins.configs.lspconfig"
-local dap = require "custom.configs.dap";
+local dap = require "custom.configs.dap"
 
 rust_tools.setup {
   server = {
-    on_attach = plugings_lsp.on_attach,
     capabilities = plugings_lsp.capabilities,
+    on_attach = function(client, bufnr)
+      plugings_lsp.on_attach(client, bufnr)
+
+      vim.keymap.set("n", "<leader>k", rust_tools.hover_actions.hover_actions, { buffer = bufnr })
+      vim.keymap.set("n", "<leader>g", rust_tools.code_action_group.code_action_group, { buffer = bufnr })
+    end,
   },
 
   dap = {
